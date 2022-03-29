d44fe73a81085894d2371cc3b24eef65d488ec79
adeninque <adilmarsov5@gmail.com>
2022-03-29 18:13:35 +0600

    completed (beta 0.1)

--- tribe-war/tribe-war.py
+++ tribe-war/tribe-war.py
@@ -12,7 +12,7 @@ class Game:
         self.game_field = [[0 for _ in range(self.__is_int(m))] for _ in range(self.__is_int(n))]
         self.__rows, self.__cols = n, m
         for tribe in tribes:
-            for _ in range(int((self.__cols * self.__rows) / 3)):
+            for _ in range(int((self.__cols * self.__rows) / 6)):
                 while True:
                     i, j = random.randint(0, self.__rows-1), random.randint(0, self.__cols-1)
                     if type(self.game_field[i][j]) != Cell:
@@ -30,28 +30,37 @@ class Game:
     
     def step(self):
         changes = []
-        for i in range(self.__rows):
-            for j in range(self.__cols):
-                neighbors = []
-                allies = 0
-                enemies = 0
+        for i in range(len(self.game_field)):
+            for j in range(len(self.game_field[i])):
+                neighbors, allies, enemies = {}, 0, 0
                 for k in range(i - 1, i + 2):
                     for l in range(j - 1, j + 2):
                         if k != i or l != j:
                             if 0 <= k < self.__rows and 0 <= l < self.__cols:
-                                if type(self.game_field[k][l]) == Cell and type(self.game_field[i][j]) == Cell:
-                                    neighbors.append(self.game_field[k][l].tribe)
-                                    if self.game_field[i][j].tribe != self.game_field[k][l].tribe:
-                                        enemies += 1
+                                if type(self.game_field[k][l]) == Cell:
+                                    if self.game_field[k][l].tribe in neighbors:
+                                        neighbors[self.game_field[k][l].tribe] += 1
                                     else:
-                                        allies += 1
-                if self.game_field[i][j] != 0:
-                    if allies > enemies:
+                                        neighbors[self.game_field[k][l].tribe] = 1
+                                    if type(self.game_field[i][j]) == Cell:
+                                        if self.game_field[k][l].tribe == self.game_field[i][j].tribe:
+                                            allies += 1
+                                        else:
+                                            enemies += 1
+
+                if type(self.game_field[i][j]) == Cell:
+                    if enemies > allies:
+                        changes.append([0, i, j])
+                    elif enemies == allies:
+                        if random.randint(0, 1):
+                            changes.append([0, i, j])
+                    elif allies >= 3 or allies <= 0:
                         changes.append([0, i, j])
                 else:
                     if neighbors:
-                        maxim = dict(Counter(neighbors))
-                        self.game_field[i][j] = Cell(max(maxim, key = maxim.get))
+                        if neighbors[max(neighbors, key = neighbors.get)] in [3,4]:
+                            changes.append([Cell(max(neighbors, key = neighbors.get)), i, j])
+
         for c in changes:
             self.game_field[c[1]][c[2]] = c[0]
     
@@ -70,7 +79,7 @@ class Game:
                     for j in range(self.__cols):
                         canva.create_rectangle(0 + (20*j), 0 + (20*i), 20 + (20*j), 20 + (20*i), fill = dead_color if self.game_field[i][j] == 0 else self.game_field[i][j].color)
             self.step()
-            canva.after(300, run)
+            canva.after(100, run)
         
         canva.pack()
         run()
@@ -78,4 +87,4 @@ class Game:
         
 
 if __name__ == '__main__':
-    Game(30, 40, "#FBCB2D", '#37FB2D', '#2DFBE3').start()
+    Game(30, 40, "ORANGE", 'GREEN', 'BLUE').start()
